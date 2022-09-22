6502418d061274d153053fcb6c8da94bdec73b6f
Keith Davis <keith.davis@arm.com>
2022-09-22 22:25:19 +0100

    Initial commit for primary FastAPI project
     * Initial creation of database models
     * Setup init.py to import all models from complaints and users
     * Initial foundations for db.py for creating and defining the database

---
+++ fastApiComplaintsSystem/db.py
@@ -0,0 +1,9 @@
+# Used for creating and defining our database
+
+import databases
+import sqlalchemy
+from decouple import config
+
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/complaints"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
